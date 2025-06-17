11679c00605568f47eda763bde1969bfbee143b6
ic3-neo <161826251+ic3-neo@users.noreply.github.com>
2025-06-17 13:08:13 +0200

    translated

--- src/types/device-setting.ts
+++ src/types/device-setting.ts
@@ -42,7 +42,7 @@ export type DeviceSettingType = Record<DeviceSettingKeyEnum, any>
 export const initDeviceSetting = {
   [DeviceSettingKeyEnum.NIGHT_LIGHTS_MODE_SET]:
   {
-    label: '飞行器夜航灯',
+    label: 'Drone Night Lights',
     value: '',
     trueValue: NightLightsStateEnum.CLOSE,
     editable: false,
@@ -50,86 +50,86 @@ export const initDeviceSetting = {
       visible: false,
       loading: false,
       // content: '为保证飞行器的作业安全，建议打开夜航灯',
-      label: '飞行器夜航灯',
+      label: 'Drone Night Lights',
     },
     settingKey: DeviceSettingKeyEnum.NIGHT_LIGHTS_MODE_SET,
   },
   [DeviceSettingKeyEnum.HEIGHT_LIMIT_SET]:
   {
-    label: '限高',
+    label: 'Height Limit',
     value: '',
     trueValue: 120,
     editable: false,
     popConfirm: {
       visible: false,
       loading: false,
-      // content: '限高：20 - 1500m',
-      // info: '修改限高会影响当前机场的所有作业任务，建议确认作业情况后再进行修改',
-      label: '限高',
+      // content: 'Height Limit：20 - 1500m',
+      // info: 'Modification of the limit will affect all the current airport tasks, it is recommended to confirm the operation situation and then modify it',
+      label: 'Height Limit',
     },
     settingKey: DeviceSettingKeyEnum.HEIGHT_LIMIT_SET,
   },
   [DeviceSettingKeyEnum.DISTANCE_LIMIT_SET]:
   {
-    label: '限远',
+    label: 'Distance Limit',
     value: '',
     trueValue: DistanceLimitStatusEnum.UNSET,
-    // info: '限远（15 - 8000m）是约束飞行器相对机场的最大作业距离',
+    // info: 'Restricted distance (15 - 8000m) is the maximum operating distance of a constraint vehicle relative to an airport',
     editable: false,
     popConfirm: {
       visible: false,
       loading: false,
-      // content: '限远 (15- 8000m) 是约束飞行器相对机场的最大作业距离',
-      // info: '修改限远会影响当前机场的所有作业任务，建议确认作业情况后再进行修改',
-      label: '限远',
+      // content: 'Range (15-8000m) is the maximum operating distance of a constrained aircraft relative to an airport',
+      // info: 'The modification limit will affect all the operational tasks of the current airport, and it is recommended to modify the operation after confirming the operation situation.',
+      label: 'Distance Limit',
 
     },
     settingKey: DeviceSettingKeyEnum.DISTANCE_LIMIT_SET,
   },
   [DeviceSettingKeyEnum.OBSTACLE_AVOIDANCE_HORIZON]:
   {
-    label: '水平避障',
+    label: 'Horizontal Limit',
     value: '',
     trueValue: ObstacleAvoidanceStatusEnum.CLOSE,
-    // info: '飞行器的避障工作状态显示，可以快速开启/关闭飞行器避障，如需进一步设置请在设备运维页面设置',
+    // info: 'The machine's obstacle avoidance status display allows for the rapid opening/off of the aircraft obstacle avoidance, and if further set up, please set it up on the equipment operation and maintenance page.',
     editable: false,
     popConfirm: {
       visible: false,
       loading: false,
-      // content: '飞行器避障是保障飞行作业安全的基础功能，建议保持飞行器避障开启',
-      label: '水平避障',
+      // content: 'Aircraft obstacle avoidance is the basic function to ensure the safety of flight operations, it is recommended to keep the aircraft obstacle avoidance open',
+      label: 'Horizontal Limits',
 
     },
     settingKey: DeviceSettingKeyEnum.OBSTACLE_AVOIDANCE_HORIZON,
   },
   [DeviceSettingKeyEnum.OBSTACLE_AVOIDANCE_UPSIDE]:
   {
-    label: '上视避障',
+    label: 'Up Limit',
     value: '',
     trueValue: ObstacleAvoidanceStatusEnum.CLOSE,
-    // info: '飞行器的避障工作状态显示，可以快速开启/关闭飞行器避障，如需进一步设置请在设备运维页面设置',
+    // info: 'The machine's obstacle avoidance status display allows for the rapid opening/off of the aircraft obstacle avoidance, and if further set up, please set it up on the equipment operation and maintenance page.',
     editable: false,
     popConfirm: {
       visible: false,
       loading: false,
-      // content: '飞行器避障是保障飞行作业安全的基础功能，建议保持飞行器避障开启',
-      label: '上视避障',
+      // content: 'Aircraft obstacle avoidance is the basic function to ensure the safety of flight operations, it is recommended to keep the aircraft obstacle avoidance open',
+      label: 'Up Limit',
 
     },
     settingKey: DeviceSettingKeyEnum.OBSTACLE_AVOIDANCE_UPSIDE,
   },
   [DeviceSettingKeyEnum.OBSTACLE_AVOIDANCE_DOWNSIDE]:
   {
-    label: '下视避障',
+    label: 'Down Limit',
     value: '',
     trueValue: ObstacleAvoidanceStatusEnum.CLOSE,
-    // info: '飞行器的避障工作状态显示，可以快速开启/关闭飞行器避障，如需进一步设置请在设备运维页面设置',
+    // info: 'The machine's obstacle avoidance status display allows for the rapid opening/off of the aircraft obstacle avoidance, and if further set up, please set it up on the equipment operation and maintenance page.',
     editable: false,
     popConfirm: {
       visible: false,
       loading: false,
-      // content: '飞行器避障是保障飞行作业安全的基础功能，建议保持飞行器避障开启',
-      label: '下视避障',
+      // content: 'Aircraft obstacle avoidance is the basic function to ensure the safety of flight operations, it is recommended to keep the aircraft obstacle avoidance open',
+      label: 'Down Limit',
 
     },
     settingKey: DeviceSettingKeyEnum.OBSTACLE_AVOIDANCE_DOWNSIDE,
@@ -137,12 +137,12 @@ export const initDeviceSetting = {
 } as DeviceSettingType
 
 export const initDeviceSettingFormModel = {
-  nightLightsState: false, // 夜航灯开关
-  heightLimit: 20, // 限高设置
-  distanceLimitStatus: { state: false, distanceLimit: 15 }, // 限远开关
-  obstacleAvoidanceHorizon: false, // 飞行器避障-水平开关设置
-  obstacleAvoidanceUpside: false, // 飞行器避障-上视开关设置
-  obstacleAvoidanceDownside: false, // 飞行器避障-下视开关设置
+  nightLightsState: false, // Nightlight switch
+  heightLimit: 20, // Limiting height setting
+  distanceLimitStatus: { state: false, distanceLimit: 15 }, // Remote switch
+  obstacleAvoidanceHorizon: false, // Aircraft obstacle avoidance - horizontal switch settings
+  obstacleAvoidanceUpside: false, // Aircraft obstacle avoidance - upper view switch settings
+  obstacleAvoidanceDownside: false, // Aircraft obstacle avoidance - down-view switch settings
 }
 
 export type DeviceSettingFormModel = typeof initDeviceSettingFormModel
